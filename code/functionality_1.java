b691acc37e435a5e73a72b2ba4f11bec0bbf464b
Ayomide Oyedele <a.oyodele@student.curtin.edu.au>
2023-04-02 15:19:44 -0400

    Update the functionality_1 code

--- code/functionality_1.java
+++ code/functionality_1.java
@@ -1,17 +1,37 @@
 public class functionality_1 {
 
-	public static boolean isFibonacci(int number) {
+	//public static boolean isFibonacci(int number) {
 
 
-    if (number == 0 || number == 1) {
-        return true;
-    }
-    int a = 0, b = 1, c = 1;
-    while (c < number) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
-    return (c == number);
+    //if (number == 0 || number == 1) {
+      //  return true;
+    //}
+    //int a = 0, b = 1, c = 1;
+    //while (c < number) {
+        //c = a + b;
+        //a = b;
+      //  b = c;
+    //}
+  //  return (c == number);
+//	}
+//}
+
+public static int fibonacci(int n) {
+
+	if (n <=1) {
+		return n;
+	}
+	
+	int prev1 = 0;
+	int prev2 = 1;
+	int current = 0;
+
+	for (int i = 2; i <= n; i++) {
+		current = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = current;
+	}
+
+	return current;
 	}
 }
