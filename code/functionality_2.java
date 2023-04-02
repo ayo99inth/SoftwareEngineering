e3a487ddc60bd7185cb5dcb806551d8f7ba068ef
Ayomide Oyedele <a.oyodele@student.curtin.edu.au>
2023-04-02 15:25:07 -0400

    Update the functionality_2 code

--- code/functionality_2.java
+++ code/functionality_2.java
@@ -1,13 +1,27 @@
 public class functionality_2 {
 	
+	//public static int getNextFibonacciNumber(int n){
+	
+	//int first = 0, second = 1, next = 1;
+	//while (next <=n){
+	//first = second;
+	//second = next;
+	//next = first + second;
+	//}
+	//return next;
+	//}
+	
 	public static int getNextFibonacciNumber(int n){
 	
-	int first = 0, second = 1, next = 1;
-	while (next <=n){
-	first = second;
-	second = next;
-	next = first + second;
-	}
-	return next;
+		int prev1 = 0, prev2 = 1;
+		
+		for (int i = 2; i <= n; i++) {
+			int current = prev1 + prev2;
+			prev1 = prev2;
+			prev2 = current;
+		}
+		
+		return prev2;
 	}
 }
+
