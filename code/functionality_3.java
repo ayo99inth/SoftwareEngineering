e5bdb678e4069f5529f541c7e2503237881a40a6
Ayomide Oyedele <a.oyodele@student.curtin.edu.au>
2023-04-02 15:30:13 -0400

    Update the functionality_3 code

--- code/functionality_3.java
+++ code/functionality_3.java
@@ -1,5 +1,17 @@
 public class functionality_3 {	
-	public static boolean isInteger(double number){
-	return Double.valueOf(number).intValue() == number;
-	}
+//	public static boolean isInteger(double number){
+//	return Double.valueOf(number).intValue() == number;
+//	}
+
+    public static boolean isInteger(double number) {
+        int n = Double.valueOf(number).intValue();
+        if (number < 0) {
+            return false;
+        }
+        if (n == number) {
+            return true;
+        }
+        return false;
+    }
 }
+
