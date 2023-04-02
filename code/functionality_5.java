e8cb523d9a8fe362dcdfbca8144acc198de1ab9e
Ayomide Oyedele <a.oyodele@student.curtin.edu.au>
2023-04-02 15:45:19 -0400

    Update the functionality_5 code

--- code/functionality_5.java
+++ code/functionality_5.java
@@ -1,9 +1,24 @@
 public class functionality_5 {
+    //public static boolean isSquare(int number) {
+        //if (number < 0) {
+         //   return false;
+       // }
+        //int sqrt = (int) Math.sqrt(number);
+        //return sqrt * sqrt == number;
+    //}
+    
     public static boolean isSquare(int number) {
         if (number < 0) {
             return false;
         }
-        int sqrt = (int) Math.sqrt(number);
-        return sqrt * sqrt == number;
+        int sqrt = 0;
+        while (sqrt * sqrt <= number) {
+            if (sqrt * sqrt == number) {
+                return true;
+            }
+            sqrt++;
+        }
+        return false;
     }
 }
+
