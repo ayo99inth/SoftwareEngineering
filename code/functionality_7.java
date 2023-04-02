64f70bb88499dcfd98c887ba74236ad524cfd4fc
Ayomide Oyedele <a.oyodele@student.curtin.edu.au>
2023-04-02 15:53:51 -0400

    Update the functionality_7 code

--- code/functionality_7.java
+++ code/functionality_7.java
@@ -1,11 +1,32 @@
 public class functionality_7 {
 
+    //public static int fibonacci(int n) {
+        //if (n <= 1) {
+         //   return n;
+        //} else {
+       //     return fibonacci(n-1) + fibonacci(n-2);
+     //   }
+    //}
+    
+    //public static void main(String[] args) {
+    //    int n = 7; // Replace with the desired input value
+  //      int result = fibonacci(n);
+//        System.out.println("The " + n + "th Fibonacci number is: " + result);
+    //}
+
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
-        } else {
-            return fibonacci(n-1) + fibonacci(n-2);
+        } 
+        
+        int a = 0, b = 1, temp;
+        for (int i = 2; i <= n; i++) {
+            temp = b;
+            b = a + b;
+            a = temp;
         }
+        
+        return b;
     }
     
     public static void main(String[] args) {
@@ -14,4 +35,5 @@ public class functionality_7 {
         System.out.println("The " + n + "th Fibonacci number is: " + result);
     }
 }
+
 
