d32a433ab4d2b92307b244f8ba96f564d115f503
Ayomide Oyedele <a.oyodele@student.curtin.edu.au>
2023-04-03 08:05:40 -0400

    Updated the functionality_4 code to take input from file Fibonacci.txt

--- code/functionality_4.java
+++ code/functionality_4.java
@@ -1,28 +1,59 @@
 import java.util.Arrays;
 import java.util.*;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class functionality_4 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    //public static void main(String[] args) {
+        //Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the size of the array: ");
-        int n = input.nextInt();
+        //System.out.print("Enter the size of the array: ");
+        //int n = input.nextInt();
 
-        int[] numbers = new int[n];
+        //int[] numbers = new int[n];
 
-        System.out.println("Enter the elements of the array: ");
-        for (int i = 0; i < n; i++) {
-            numbers[i] = input.nextInt();
-        }
+        //System.out.println("Enter the elements of the array: ");
+        //for (int i = 0; i < n; i++) {
+          //  numbers[i] = input.nextInt();
+        //}
 
         // Call the isFibonacci method from the Fibonacci class
-        boolean[] result = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = functionality_1.isFibonacci(numbers[i]);
-        }
+        //boolean[] result = new boolean[n];
+        //for (int i = 0; i < n; i++) {
+        //    result[i] = functionality_1.isFibonacci(numbers[i]);
+      //  }
 
-        System.out.println("Result: " + Arrays.toString(result));
+    //    System.out.println("Result: " + Arrays.toString(result));
+  //  }
+//}
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("Fibonacci.txt");
+            Scanner scanner = new Scanner(file);
+
+            int n = scanner.nextInt();
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                numbers[i] = scanner.nextInt();
+            }
+
+            // Call the isFibonacci method from the Fibonacci class
+            boolean[] result = new boolean[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = functionality_1.isFibonacci(numbers[i]);
+            }
+
+            System.out.println("Result: " + Arrays.toString(result));
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
 
